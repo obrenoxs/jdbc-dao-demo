d54ea01749e8636c51b0f086598ff039b6d854a5
Breno <breno.joaosalvadorperez200@gmail.com>
2026-06-04 19:57:01 -0300

    DepartmentDao and SellerDao interfaces

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -98,6 +98,4 @@ public class Seller implements Serializable{
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-	
-	
 }
